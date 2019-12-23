22102d0b6932790149c7998abb000a16ab48598b
Iqbaldhia <r13.aero@yahoo.co.id>
2019-12-23 10:10:00 +0700

    add advanced pagination

--- src/Models/utility/pagination.js
+++ src/Models/utility/pagination.js
@@ -20,26 +20,43 @@ module.exports = {
         }
       }
 
-        const sql = `
+      const sql = `
         SELECT count(engineer.id) as total_data 
         FROM engineer ${queryString}
         `;
 
       db.query(sql, (err, result) => {
         if (!err) {
-          console.log(result)
-
           const total_page = Math.ceil(result[0].total_data / limit);
           const current_page = query.page || 1;
-          const next_page = total_page - current_page
-          const prev_page = ( total_page - 1 ) - next_page
+          const next_page = total_page - current_page;
+          const prev_page = total_page - 1 - next_page;
+          let paginationArrPrev = []
+          let paginationArrNext = []
+          let paginationIllustrate = []
+          
+          // Prev page
+          for (let index = 1; index < current_page; index++) {
+            paginationArrPrev.push(index)
+            paginationIllustrate.push(index)
+          }
 
+          // Current page
+          paginationIllustrate.push(`[ ${current_page} ]`)
+
+          // Next Page
+          for(let index = (parseInt(current_page)+1); index <= total_page; index++){
+            paginationArrNext.push(index)
+            paginationIllustrate.push(index)
+          }
+
+          // Set result
           result[0].total_page = Math.ceil(result[0].total_data / limit);
-          result[0].current_page = current_page
-          result[0].next_page = next_page
-          result[0].prev_page = prev_page
+          result[0].current_page = current_page;
+          result[0].prev_page = paginationArrPrev;
+          result[0].next_page = paginationArrNext;
+          result[0].pagination = paginationIllustrate
 
-          // console.log(result)
           resolve(result);
         } else {
           reject(err);
